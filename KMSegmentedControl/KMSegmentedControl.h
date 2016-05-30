be8a9716fb81df95b1e8007e84e5a49197d8d079
Kamil Mazurek <kamilm@outlook.de>
2016-05-30 17:21:30 +0200

    Initial Commit

---
+++ KMSegmentedControl/KMSegmentedControl.h
@@ -0,0 +1,19 @@
+//
+//  KMSegmentedControl.h
+//  KMSegmentedControl
+//
+//  Created by Kamil Mazurek on 30/05/16.
+//  Copyright © 2016 KamilMazurek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KMSegmentedControl.
+FOUNDATION_EXPORT double KMSegmentedControlVersionNumber;
+
+//! Project version string for KMSegmentedControl.
+FOUNDATION_EXPORT const unsigned char KMSegmentedControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KMSegmentedControl/PublicHeader.h>
+
+
