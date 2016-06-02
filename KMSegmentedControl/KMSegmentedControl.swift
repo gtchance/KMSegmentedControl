6d3efc916b447e43dcd4a6ae62c28b6507ee31a4
Kamil Mazurek <kamilm@outlook.de>
2016-06-02 18:35:23 +0200

    fix

--- KMSegmentedControl/KMSegmentedControl.swift
+++ KMSegmentedControl/KMSegmentedControl.swift
@@ -108,7 +108,7 @@ public protocol KMSegmentedControlDelegate {
 
     // MARK : Initialitation
 
-  override init(frame: CGRect) {
+  override public init(frame: CGRect) {
     super.init(frame: frame)
     setupKMSegmentedControl()
   }
